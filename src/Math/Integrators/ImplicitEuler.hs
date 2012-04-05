ae9169b0fc9cdf1233a73cf4f7c02aca01e2e7b7
Alexander Vershilov <alexander.vershilov@gmail.com>
2012-04-05 20:05:57 +0400

    implicit euler method

---
+++ src/Math/Integrators/ImplicitEuler.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE FlexibleContexts #-}
+module Math.Integrators.ImplicitEuler
+	where
+
+import Data.Function
+import Data.VectorSpace
+
+import Math.Integrators.Implicit
+import Math.Integrators.Internal
+
+eps = 1e-14::Double
+
+implicitEuler :: (VectorSpace a, Floating (Scalar a)) => (a -> a) -> (a -> Double) -> Integrator a
+implicitEuler f norm = \h y -> fixedPoint (\x -> y ^+^ (realToFrac h) *^ (f x)) (\x1 x2 -> breakNormR eps (norm (x1^-^x2))) y
