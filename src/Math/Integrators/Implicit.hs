6995e0862360d431d669d9029de1b5a856459462
Alexander Vershilov <alexander.vershilov@gmail.com>
2012-04-17 09:56:46 +0400

    small fixes in integrator

--- src/Math/Integrators/Implicit.hs
+++ src/Math/Integrators/Implicit.hs
@@ -1,27 +1,45 @@
+{-# LANGUAGE FlexibleContexts, TypeFamilies #-}
 -- | Helpers for implicit integration methods
 --
+-- TODO: add possibility to make function to create initial value
 -- TODO: add possibility to break on step
 -- TODO: add possibility to add different initial value based
 --          on y0, f
 -- TODO: add seq-pseq to make this stuff strict
 -- TODO: add Newton iterations
 module Math.Integrators.Implicit
+    ( -- * types
+      ImplicitSolver
+      -- * solvers
+    , fixedPointSolver
+    , fixedPoint
+      -- * helpers
+    , breakNormR
+    , breakNormIR
+    )
     where
 
--- | simple break rule that will break evaluatioin when value less then Eps
-breakNormR :: Double -> Double -> Bool
-breakNormR eps y =  y < eps
+import Data.VectorSpace
 
--- | same as @breakNormR@ but assume that inner type is an 
--- instance of InnerField, so it's possible to use innerproduct to find norm
--- N.B function uses $||v||^2 < eps$, so epsilon should be pre evaluated
-breakNormIR :: (InnerSpace a, VectorSpace (Floating b)) => a -> b -> Bool
-breakNormIR v eps = (v <.> v) < eps
-
+-- | Implicit solver type
+type ImplicitSolver a = (a -> a)                    -- ^ implicit method
+                        -> (Int -> a -> a -> Bool)  -- ^ breakRule
+                        -> a                        -- ^ initial value
+                        -> a                        -- ^ final value
 
 -- | Fixed point method it iterates function f until it will break "" will
 -- be reached then it returns one but last iteration
 --
+{-# DEPRECATED fixedPoint "will be removed in future versions" #-}
+fixedPointSolver :: ImplicitSolver a
+fixedPointSolver f break y0 = inner 0 y0
+    where 
+        inner i y = let y' = f y
+                        i' = i+1
+                    in if break i y y'
+                           then y'
+                           else inner i' y'
+
 fixedPoint :: (a -> a)            -- ^ function
               -> (a -> a -> Bool) -- ^ break rule
               -> a                -- ^ initial value
@@ -31,3 +49,18 @@ fixedPoint f break y0 =
     in if break y0 y1
         then y0
         else fixedPoint f break y1
+maxSteps :: Int -> (a -> a -> Bool) -> (Int -> a -> a -> Bool)
+maxSteps n f = \i y y' -> (i>n) || f y y'
+
+-- | simple break rule that will break evaluatioin when value less then Eps
+breakNormR :: Double -> Double -> Bool
+breakNormR eps y =  y < eps
+
+-- | same as @breakNormR@ but assume that inner type is an 
+-- instance of InnerField, so it's possible to use innerproduct to find norm
+-- N.B function uses $||v||^2 < eps$, so epsilon should be pre evaluated
+breakNormIR :: (InnerSpace a, Scalar a ~ Double) => a -> Double -> Bool
+breakNormIR v eps = (v <.> v) < eps
+
+
+
--- src/Math/Integrators/StormerVerlet.hs
+++ src/Math/Integrators/StormerVerlet.hs
@@ -1,10 +1,15 @@
 {-# LANGUAGE FlexibleContexts #-}
--- Stormer-Verlet has next properties
--- * the method is of order 2
--- * this is sympletic method
--- * method is symmetric
--- * ? for separate Hamiltonians T(p) + U(v) this method is explicit //not in code
--- * this method excatly conserves quadratic first integrals $p^T C$ (angular momentum of N-body system)
+-- | Stormer-Verlet has next properties
+--
+--      * the method is of order 2
+--
+--      * this is sympletic method
+--
+--      * method is symmetric
+--
+--      * ? for separate Hamiltonians T(p) + U(v) this method is explicit //not in code
+--
+--      * this method excatly conserves quadratic first integrals $p^T C$ (angular momentum of N-body system)
 --
 
 
@@ -15,18 +20,14 @@ import Data.AdditiveGroup
 import Data.VectorSpace
 
 import Math.Integrators.Internal
--- | St\"ormer-Verlet integration scheme for equatio q''=f(q)
+
+
+-- | Stormer-Verlet integration scheme for system: 
+--      
+--      q'' = f(q)
 --
--- p_{n+1/2} = p_{n-1/2} + h *f(q)
--- stormerVerlet1 :: (VectorSpace a, Floating (Scalar a)) -> (a->a) -> Integrator a
--- stormerVerlet1 = \h p -> -- undefined
-    
-
-
--- | St\"ormer-Verlet integration scheme for system:
--- p'  = f(q)
--- q' = p
-stormerVerlet2 :: (VectorSpace a, Floating (Scalar a)) => (a -> a) -> Integrator (a,a)
+stormerVerlet2 :: (VectorSpace a, Floating (Scalar a)) => (a -> a)              -- ^ function f
+                                                       -> Integrator (a,a)      -- ^ output integrator
 stormerVerlet2 f = \h (p,q) -> 
     let h'  = realToFrac h
         h2' = realToFrac $! 0.5 *h
