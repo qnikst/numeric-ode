d3379fe5430c70b971d0513931e23002f0df0488
Dominic Steinitz <dominic@steinitz.org>
2017-07-16 10:28:17 +0100

    Draw gif of one planet.

--- src/Examples/KeplerProblem.hs
+++ src/Examples/KeplerProblem.hs
@@ -1,9 +1,5 @@
 {-# OPTIONS_GHC -Wall         #-}
 
-{-# LANGUAGE BangPatterns     #-}
-{-# LANGUAGE TypeFamilies     #-}
-{-# LANGUAGE FlexibleContexts #-}
-
 module Main (main) where
 
 import qualified Data.Vector as V
@@ -15,16 +11,29 @@ import Math.Integrators
 
 import qualified Linear as L
 
-import Graphics.Rendering.Chart
-import Graphics.Rendering.Chart.Backend.Cairo
-import Data.Colour.Names
-import Data.Colour
-import Data.Default.Class
+import Diagrams.Prelude
+import Diagrams.Backend.CmdLine
+import Diagrams.Backend.Rasterific.CmdLine
+import Codec.Picture.Gif
 
-import Diagrams.Prelude ((.~))
+import Plots
 
-kepler' :: L.V2 (L.V3 Double) -> L.V2 (L.V3 Double)
-kepler' (L.V2 q1 q2) =
+
+myaxis :: Axis B V2 Double
+myaxis = r2Axis &~ do
+  linePlot' $ map unp2 $ take 200 morePts
+
+myaxis' :: Int -> Axis B V2 Double
+myaxis' n = r2Axis &~ do
+  linePlot (map unp2 $ take n morePts) $ do
+    plotColor .= black
+  xMin .= Just (-6.00)
+  xMax .= Just ( 0.00)
+  yMin .= Just (-1.01)
+  yMax .= Just ( 0.40)
+
+kepler :: L.V2 (L.V3 Double) -> L.V2 (L.V3 Double)
+kepler (L.V2 q1 q2) =
     let r  = q2 L.^-^ q1          -- q2 - q1
         ri = r `L.dot` r          -- ||q2-q1||^2
         rr = ri * (sqrt ri)
@@ -32,47 +41,49 @@ kepler' (L.V2 q1 q2) =
         q2' = negate q1'
     in L.V2 q1' q2'
 
-k2p' :: L.V3 Double ->
+k2p :: L.V3 Double ->
         (L.V2 (L.V3 Double), L.V2 (L.V3 Double)) ->
         (L.V2 (L.V3 Double), L.V2 (L.V3 Double))
-k2p' = \h (v,x) ->
-         let v' = (implicitEuler (\_ -> kepler' x)) h v
+k2p = \h (v,x) ->
+         let v' = (implicitEuler (\_ -> kepler x)) h v
              x' = (implicitEuler (\_ -> v')) h x
          in (v',x')
 
 tm :: V.Vector Double
 tm = V.enumFromStepN 0 0.0001 1000000
 
-u :: (L.V2 (L.V3 Double), L.V2 (L.V3 Double))
-u = ((L.V2 (L.V3 0.1 0 0) (L.V3 (-0.1) 0 0)), (L.V2 (L.V3 (-1) (-1) 0) (L.V3 1 1 0)))
+-- u :: (L.V2 (L.V3 Double), L.V2 (L.V3 Double))
+-- u = ((L.V2 (L.V3 1.2 0 0) (L.V3 (-0.9) 0 0)), (L.V2 (L.V3 (-1) (-1) 0) (L.V3 1 1 0)))
 
 u' :: L.V2 (L.V2 (L.V3 Double))
-u' = L.V2 (L.V2 (L.V3 0.1 0 0) (L.V3 (-0.1) 0 0)) (L.V2 (L.V3 (-1) (-1) 0) (L.V3 1 1 0))
+u' = L.V2 (L.V2 (L.V3 0.1 0 0) (L.V3 (-0.5) 0 0)) (L.V2 (L.V3 (-1.5) (-1.0) 0) (L.V3 0.5 1.0 0))
 
 result1 :: V.Vector (L.V2 (L.V2 (L.V3 Double)))
-result1 = runST $ integrateV (\h -> stormerVerlet2 kepler' (pure h)) u' tm
+result1 = runST $ integrateV (\h -> stormerVerlet2 kepler (pure h)) u' tm
 
-result2 :: V.Vector (L.V2 (L.V3 Double), L.V2 (L.V3 Double))
-result2 = runST $ integrateV (\h -> k2p' (pure h)) u tm
+-- result2 :: V.Vector (L.V2 (L.V3 Double), L.V2 (L.V3 Double))
+-- result2 = runST $ integrateV (\h -> k2p (pure h)) u tm
 
-plotA :: V.Vector (L.V2 (L.V2 (L.V3 Double))) -> IO (PickFn ())
-plotA r = renderableToFile def "../diagrams/integrators_example1_big.png" mychart
-    where
-        mychart :: Renderable ()
-        mychart = toRenderable layout
-        layout  = layout_title .~ "bugs"
-                $ layout_plots .~ [toPlot plot1 ,toPlot plot2]
-                $ def
-        plot1  = plot_lines_style . line_color .~ opaque blue
-               $ plot_lines_title  .~ "body-1"
-               $ plot_lines_values .~ [map (\(L.V2 _ (L.V2 (L.V3 x y _z) _)) -> (x,y))  (V.toList r)]
-               $ def
-        plot2  = plot_lines_style . line_color .~ opaque red
-               $ plot_lines_title .~ "body-2"
-               $ plot_lines_values .~ [map (\(L.V2 _ (L.V2 _ (L.V3 x y _z))) -> (x,y))  (V.toList r)]
-               $ def
+each' :: Int -> [a] -> [a]
+each' n = map head . takeWhile (not . null) . iterate (drop n)
+
+morePts :: [P2 Double]
+morePts = map p2 $ each' 1000 $ map (\(L.V2 _ (L.V2 (L.V3 x y _z) _)) -> (x,y))  (V.toList result1)
+
+displayHeader :: FilePath -> [(Diagram B, GifDelay)] -> IO ()
+displayHeader fn =
+  mainRender ( DiagramOpts (Just 900) (Just 700) fn
+             , GifOpts {_dither = False, _noLooping = False, _loopRepeat = Just 10}
+             )
+
+displayHeader' :: FilePath -> Diagram B -> IO ()
+displayHeader' fn =
+  mainRender ( DiagramOpts (Just 900) (Just 700) fn
+             , DiagramLoopOpts False Nothing 0
+             )
 
 main :: IO ()
 main = do
-  _ <- plotA result1
+  displayHeader' "other/plot-line.png" (renderAxis myaxis # bg white)
+  displayHeader "other/anim-line.gif" $ zip (map (bg white . renderAxis . myaxis') [0..80]) (repeat 10)
   putStrLn "Finished"
